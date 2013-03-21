1057c186f4c8a4fcef64d592f5dbd868318b399a
romario13 <romario13@gmail.com>
2013-03-21 11:34:15 +0400

    Added existing content

---
+++ js/tag-markdown.js
@@ -0,0 +1,35 @@
+angular.module('markdown', []).
+    directive('markdown', function ($http) {
+        var converter = new Showdown.converter();
+        return {
+            restrict: 'E',
+            scope: {
+                src: "@src"
+            },
+            link: function (scope, element, attrs) {
+                var text = element.html();
+                var htmlText = converter.makeHtml(text);
+                element.html(htmlText);
+
+                scope.$watch("src", function (value) {
+
+                    if (attrs.src) {
+
+                        $http({
+                            url: attrs.src,
+                            method: "GET"
+                        }).success(function (text) {
+                                var htmlText = converter.makeHtml(text);
+                                element.html(htmlText);
+
+                                // refresh affix spy function on nre DOM model
+                                $('[data-spy="affix"]').each(function () {
+                                    $(this).affix('refresh')
+                                });
+                            });
+
+                    }
+                });
+            }
+        };
+    })
