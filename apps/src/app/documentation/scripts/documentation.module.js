490bb6016a0113414895cf77f5710217949de194
moroz-vladimir <morozvv83@gmail.com>
2013-10-10 19:09:48 +0400

    Для приложения "Документация" добавил динамическую смену Markdown документов в правой части по клику на пункт меню в sideBar

--- apps/src/app/documentation/scripts/documentation.module.js
+++ apps/src/app/documentation/scripts/documentation.module.js
@@ -9,10 +9,28 @@ roadMapApp.config(function ($stateProvider, markdownModProvider) {
                 templateUrl: 'app/documentation/views/sidebar.html'
             },
             '': {
-                templateUrl: 'app/documentation/views/content.html'
+                templateUrl: 'app/documentation/views/content.html',
+                controller: function ($scope) {   //--Выносим сюда общие модели для дочерних блоков
+                    markdownModProvider.setSource('md/documentation/intro.md');
+                }
             }
         }
     };
-    $stateProvider.state('documentation', page);
-    markdownModProvider.setSource('md/documentation/documentation.md');
+    $stateProvider.state('documentation', page)
+        .state('documentation_section', {
+            url: '/{sectionId}', // root route
+            views: {
+                'sideBar': {
+                    templateUrl: 'app/documentation/views/sidebar.html'
+                },
+                '': {
+                    templateUrl: 'app/documentation/views/content.html',
+                    controller: function ($scope, $stateParams) {   //--Выносим сюда общие модели для дочерних блоков
+                        markdownModProvider.setSource('md/documentation/' + $stateParams.sectionId + '.md');
+                    }
+                }
+            }
+        });
+
 });
+
